1c9ea1525b9a9b908a8b4a74566f162b9a422aa1
Daniel Takamori <pono@osuosl.org>
2015-09-24 10:50:12 -0700

    add build script

---
+++ scripts/web1_build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Update repo
+git checkout master
+git pull -q --ff-only
+git submodule update --init --recursive
+
+# Enable venv
+if [ ! -d venv ]; then
+    virtualenv-2.7 venv
+fi
+source venv/bin/activate
+
+# Update packages
+pip install -r requirements.txt
+pip install pelican
+
+# Build the site
+make publish
+
+# Disable venv
+deactivate
