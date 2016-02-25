f557fd908f7cd0ff08c0da7580eb125a2ee683d5
Aileen Thai <thai@osuosl.org>
2016-02-25 12:41:04 -0800

    Prep for staging

--- stagingconf.py
+++ stagingconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://wwwstaging.osuosl.org'
+SITEURL = 'http://osuosl.staging.osuosl.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
